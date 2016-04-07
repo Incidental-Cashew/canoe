d8f03a18402ecdf5313d648551e0c3588826d6e0
Dylan Kuang <kuangd.usfcaubms@gmail.com>
2016-04-07 10:52:14 -0700

    Add redirect route to mainView/MainCtrl

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,15 @@
-// CLIENT APP
+angular.module('canoe', [
+	'canoe.main',
+	'ngRoute'
+])
+
+.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'app/mainView/mainView.html',
+			controller: 'MainCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
+});
