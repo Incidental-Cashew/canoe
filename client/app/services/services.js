907ff9eaeb3a09ffb67f93659542605dea0ccd07
Dylan Kuang <kuangd.usfcaubms@gmail.com>
2016-04-07 10:10:10 -0700

    Begin to implement getNearbyRides

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -5,16 +5,27 @@ angular.module('MODULENAME', [])
 	var getUberEstimates = function(userData) {
 		return $http({
 			url: 'https://api.uber.com/v1/estimates/price',
-			params: {
+			data: {
 				server_token: 'SERVERTOKEN',
 				start_latitude: 37.773972, //userData.startLat
 				start_longitude: -122.431297, //userData.startLong
 				end_latitude: 37.2358, //userData.endLat
-				end_longitude: 121.9624 //userData.endLong
+				end_longitude: -121.9624 //userData.endLong
 			}
 		}).then(function(res) {
 			//RESPONSE
 		});
+	};
+
+	var getNearbyRides = function() {
+		return $http({
+			method: 'POST',
+			url: 'https://api.uber.com/v1/requests', //SANDBOX: https://sandbox-api.uber.com//v1/sandbox/requests/
+			data: {
+
+			}
+		});
+
 	};
 
 	return {
