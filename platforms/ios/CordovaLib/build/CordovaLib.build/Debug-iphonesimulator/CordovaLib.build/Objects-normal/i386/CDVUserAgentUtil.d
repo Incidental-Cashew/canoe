e04c549aac2fc60987239a0ef501479a647c4bd9
Biletnikoff <bogere.sanders@gmail.com>
2016-04-07 12:36:48 -0700

    update

---
+++ platforms/ios/CordovaLib/build/CordovaLib.build/Debug-iphonesimulator/CordovaLib.build/Objects-normal/i386/CDVUserAgentUtil.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/mrmac/Development/HR/canoe/canoe/platforms/ios/CordovaLib/Classes/CDVUserAgentUtil.m \
+  /Users/mrmac/Development/HR/canoe/canoe/platforms/ios/CordovaLib/Classes/CDVUserAgentUtil.h
---
+++ platforms/ios/CordovaLib/build/CordovaLib.build/Debug-iphonesimulator/CordovaLib.build/Objects-normal/i386/NSDictionary+Extensions.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/mrmac/Development/HR/canoe/canoe/platforms/ios/CordovaLib/Classes/NSDictionary+Extensions.m \
+  /Users/mrmac/Development/HR/canoe/canoe/platforms/ios/CordovaLib/Classes/NSDictionary+Extensions.h \
+  /Users/mrmac/Development/HR/canoe/canoe/platforms/ios/CordovaLib/Classes/CDVAvailabilityDeprecated.h
