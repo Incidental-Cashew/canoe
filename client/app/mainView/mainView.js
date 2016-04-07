2bf5a8fdd8c3b45b311310c9dbea3385f079eaa3
Biletnikoff <bogere.sanders@gmail.com>
2016-04-07 16:18:26 -0700

    bind lyft driver data to $scope

--- client/app/mainView/mainView.js
+++ client/app/mainView/mainView.js
@@ -3,5 +3,7 @@ angular.module('canoe.main', [])
 .controller('MainCtrl', function($scope) {
 
 	$scope.testText = 'Compare realtime data between Uber and Lyft';
-
+	$scope.getLyftDrivers = getLyftDriversNearBy().then(function(data){
+		$scope.drivers = data.nearbydrivers.drivers.location;
+	})
 });
