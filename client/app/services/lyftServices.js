6d27f4f2adc383c20755254266e94f87958c5224
clardizabal <chris.lardizabal@gmail.com>
2016-04-07 19:40:09 -0700

    get lyft cost estimates and eta

--- client/app/services/lyftServices.js
+++ client/app/services/lyftServices.js
@@ -1,27 +1,31 @@
 // ADD LYFT FACTORY
 angular.module('canoe.lyftServices', [])
 
-.factory('Auth', function($http) {
+.factory('Auth', function($http, $window) {
   
+  var postman = 'Basic T1RheS12M2RjMFJmOmYwZlFfYlBwbTFYV0M2N0k0Yzg2TldvazRVN2pDaXpj';
+
   var getLyftToken = function() {
     return $http({
       url: 'https://api.lyft.com/oauth/token',
       method: 'POST',
       headers: {
-        'content-type': 'application/json'
+        'content-type': 'application/json',
+        authorization: postman
       },
       data: {
         grant_type: 'client_credentials',
         scope: 'public'
-      },
-      params: {
-        oauth_consumer_key: 'OTay-v3dc0Rf',
-        oauth_signature: 'f0fQ_bPpm1XWC67I4c86NWok4U7jCizc%26'
       }
     }).then(function(res) {
-      return res.access_token;
+      console.log(res.data.access_token);
+      $window.token = res.data.access_token;
+      return res.data;
     });
   };
+
+  return {getLyftToken: getLyftToken};
+
 })
 
 .factory('Details', function($http) {
@@ -31,16 +35,16 @@ angular.module('canoe.lyftServices', [])
       url: 'https://api.lyft.com/v1/cost',
       method: 'GET',
       headers: {
-        Authorization: token
+        authorization: 'bearer ' + token
       },
-      data: {
+      params: {
         start_lat: 37.773972,
         start_lng: -122.431297,
         end_lat: 37.2358, 
         end_lng: -121.9624 
       }
     }).then(function(res) {
-        //RESPONSE
+      return res.data;
     });
   };
 
@@ -49,14 +53,19 @@ angular.module('canoe.lyftServices', [])
       url: 'https://api.lyft.com/v1/eta',
       method: 'GET',
       headers: {
-        Authorization: token
+        authorization: 'bearer ' + token
       },
-      data: {
+      params: {
         lat: 37.773972,
         lng: -122.431297
       }
     }).then(function(res) {
-        //RESPONSE
+      return res.data;
     });
+  };
+
+  return {
+    getLyftEstimates: getLyftEstimates,
+    getLyftEta: getLyftEta
   };
 });
