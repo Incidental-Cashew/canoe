60bbcb02c4bdcbfafb454ce12bb9c0467e9e6904
Dylan Kuang <kuangd.usfcaubms@gmail.com>
2016-04-11 12:02:09 -0700

    Fix Uber services/auth

--- www/app/mainView/mainView.js
+++ www/app/mainView/mainView.js
@@ -1,6 +1,37 @@
 angular.module('canoe.main', [])
 
-.controller('MainCtrl', function($scope, Auth, Details) {
+.controller('MainCtrl', function($scope, UberDetails, UberAuth, Auth, Details) {
+
+  //////////////////UBER DETAILS/////////////////////
+  $scope.uberBearer;
+  $scope.uberRefresh;
+
+  UberAuth.getUberToken(getParameterByName('code')).then(function(token) {
+    console.log(token);
+    $scope.uberBearer = token.access_token;
+    $scope.uberRefresh = token.refresh_token;
+  });
+
+  $scope.getUberPriceEstimates = UberDetails.getUberPriceEstimates;
+
+  $scope.getUberTimeEstimates = function() {
+    UberDetails.getUberTimeEstimates($scope.uberBearer).then(function(estimates) {
+      // DO SOMETHING WITH TIME ESTIMATES
+    });
+  };
+
+  $scope.getUserHistory = function() {
+    UberDetails.getUserHistory($scope.uberBearer).then(function(history) {
+      //do something with user history
+    });
+  };
+
+  $scope.getNearbyRides = function() {
+    UberDetails.getNearbyRides($scope.uberBearer).then(function(rides) {
+      // do something with nearby rides
+    });
+  };
+  ///////////////////////////////////////////////
 
   Auth.getLyftToken().then(function(token) {
     // $scope.token = token.access_token;
