75bdfffa00b7e40a3c1cedd861f9b3a4dceb14f6
Biletnikoff <bogere.sanders@gmail.com>
2016-04-12 11:32:32 -0700

    add destination and pickup list experimental

--- www/controllers.js
+++ www/controllers.js
@@ -7,17 +7,18 @@ angular.module('canoe.controllers', ['ngMap', 'google.places'])
   }
 }])
 .controller('ChatsCtrl', function($scope, NgMap) {
-
+ var geocoder = new google.maps.Geocoder;
 var options = {enableHighAccuracy: true};
 
 // this will be used later
 navigator.geolocation.getCurrentPosition(function(pos) {
-                $scope.position = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-                console.log(JSON.stringify($scope.position));
-            },
-            function(error) {
-                alert('Unable to get location: ' + error.message);
-            }, options);
+  $scope.position = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+  console.log(JSON.stringify($scope.position));
+  geocoder.geocode({'location': $scope.position}, function(result, status){
+      $scope.geodecoded = result[0].formatted_address.slice(0,-30);
+  });
+
+ });
 })
 .controller('DashCtrl', function($scope, LyftAuth, LyftDetails, UberDetails) {
 
