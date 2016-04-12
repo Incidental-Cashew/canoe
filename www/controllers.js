a644db2b9257b958c1d56cad066ff4b88516bf95
Biletnikoff <bogere.sanders@gmail.com>
2016-04-12 14:32:20 -0700

    modal view search feature

--- www/controllers.js
+++ www/controllers.js
@@ -38,9 +38,9 @@ navigator.geolocation.getCurrentPosition(function(pos) {
         console.log(value.eta_estimates);
 
         // ADD ETA to Lyft Estimates
-        $scope.lyftEstimates.forEach(function(ride, index) {
-          ride.eta_seconds = value.eta_estimates[index].eta_seconds;
-        });
+        // $scope.lyftEstimates.forEach(function(ride, index) {
+        //   ride.eta_seconds = value.eta_estimates[index].eta_seconds;
+        // });
       });
     });
 
@@ -54,10 +54,32 @@ navigator.geolocation.getCurrentPosition(function(pos) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
+.controller('ModalCtrl', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('./templates/searchModel.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    console.log('FIRE');
+    $scope.modal.show();
   };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 })
 
 .controller('MainCtrl', function ($scope) {
@@ -125,30 +147,4 @@ navigator.geolocation.getCurrentPosition(function(pos) {
 
 
 // controls the modal for setting a location/destination
-.controller('ModalCtrl', function($scope, $ionicModal) {
-  $ionicModal.fromTemplateUrl('./templates/searchModel.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-  $scope.openModal = function() {
-    console.log('FIRE');
-    $scope.modal.show();
-  };
-  $scope.closeModal = function() {
-    $scope.modal.hide();
-  };
-  //Cleanup the modal when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
-  // Execute action on hide modal
-  $scope.$on('modal.hidden', function() {
-    // Execute action
-  });
-  // Execute action on remove modal
-  $scope.$on('modal.removed', function() {
-    // Execute action
-  });
-});
+;
